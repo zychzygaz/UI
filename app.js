bb6df57518a66ed23f815de2707bb4ec53234e90
zychzygaz <78308744+zychzygaz@users.noreply.github.com>
2021-03-20 11:25:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form");
+const addItem = document.getElementById("addItem");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (addItem.value === "") {
+        addItem.className = "error";
+        document.querySelector("#small").className = "smallError";
+    } else if (addItem.value != "") {
+        addItem.className = "passed";
+        document.querySelector("#small").className = "unvis";
+
+        const listOfItems = document.getElementById("listOfItems");
+        const item = document.createElement("li");
+        const delTag = document.createElement("a");
+        item.innerHTML = addItem.value;
+        delTag.innerHTML = '<i class="fas fa-trash"></i>';
+        delTag.className = "delete-item secondary-content";
+
+        item.appendChild(delTag);
+        listOfItems.appendChild(item);
+
+        delTag.addEventListener("click", () => {
+            item.parentNode.removeChild(item);
+        });
+
+        addItem.value = "";
+    }
+});
